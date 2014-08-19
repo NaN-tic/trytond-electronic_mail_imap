57d541aaca9557defef8983d8bd7cbe6475bb2e7
Bernat Brunet Torruella <bernat@nan-tic.com>
2014-08-19 17:44:58 +0200

    Add rais message and update button function

--- imap.py
+++ imap.py
@@ -4,7 +4,7 @@ from trytond.pool import Pool, PoolMeta
 from trytond.model import ModelView, fields
 from trytond.pyson import Eval
 
-import email
+from email import message_from_string
 import chardet
 import logging
 
@@ -21,8 +21,11 @@ class IMAPServer:
     @classmethod
     def __setup__(cls):
         super(IMAPServer, cls).__setup__()
+        cls._error_messages.update({
+                'invalid_state_server': 'IMAP server "%s" is in draft state',
+                })
         cls._buttons.update({
-            'get_mails': {
+            'get_emails': {
                 'invisible': Eval('state') == 'draft',
                 },
             })
@@ -32,25 +35,30 @@ class IMAPServer:
     def get_emails(cls, servers):
         "Get emails from server and save like ElectronicMail module"
         ElectronicMail = Pool().get('electronic.mail')
+        mails = {}
         for server in servers:
-            try:
+            mails[server.id] = []
+            if server.state != 'draft':
                 imapper = cls.connect(server)
                 messages = cls.fetch(imapper, server)
-            except Exception, e:
-                cls.raise_user_error('general_error', e)
-            logging.getLogger('IMAPServer').info(
-                    'Process %s email(s) from %s' % (
-                    len(messages),
-                    server.name,
-                    ))
-            for message in messages:
-                msg = message[0][1]
-                if not isinstance(msg, str):
-                    encoding = chardet.detect(message)
-                    message = message.decode(encoding.get('encoding'))
-                # Warning: 'message_from_string' doesn't always work correctly
-                # on unicode, we must use utf-8 strings.
-                if isinstance(msg, unicode):
-                    msg = msg.encode('utf-8')
-                mail = email.message_from_string(msg)
-                ElectronicMail.create_from_email(mail, servers.mailbox)
+                logging.getLogger('IMAPServer').info(
+                        'Process %s email(s) from %s' % (
+                        len(messages),
+                        server.name,
+                        ))
+                for message_id, message in messages.iteritems():
+                    msg = message[0][1]
+                    if not isinstance(msg, str):
+                        encoding = chardet.detect(msg)
+                        msg = msg.decode(encoding.get('encoding'))
+                    # Warning: 'message_from_string' doesn't always work
+                    # correctly on unicode, we must use utf-8 strings.
+                    if isinstance(msg, unicode):
+                        msg = msg.encode('utf-8')
+                    mail = message_from_string(msg)
+                    mails[server.id].append(ElectronicMail.create_from_email(
+                            mail, server.mailbox))
+            else:
+                cls.raise_user_error('invalid_state_server',
+                    error_args=(server.state,))
+        return mails
